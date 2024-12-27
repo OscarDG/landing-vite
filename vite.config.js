b875f72ceefaea2963c9b0b61bd83cb390f6e883
OscarDG <oscard.guerrerom@outlook.com>
2024-12-27 00:19:16 -0500

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://oscardg.github.io/landing-vite/'
+  base: "/landing-vite/"
 });
 
